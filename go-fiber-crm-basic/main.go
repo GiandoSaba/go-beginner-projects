acd7cdcd92bb6d7475d69cb1897d7e1665ac395a
GiandoSaba <giandosaba997@gmail.com>
2024-04-05 19:04:39 +0200

    dev go-fiber-crm-basic

---
+++ go-fiber-crm-basic/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"giandosaba/go-fiber-crm-basic/database"
+	"giandosaba/go-fiber-crm-basic/lead"
+
+	"github.com/gofiber/fiber"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("api/v1/lead", lead.GetLeads)
+	app.Get("api/v1/lead/:id", lead.GetLead)
+	app.Post("api/v1/lead", lead.NewLead)
+	app.Delete("api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open(sqlite.Open("leads.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+}
